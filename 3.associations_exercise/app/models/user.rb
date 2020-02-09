581f646ff9fff9881398b4162fbcc2b8f6bf3499
fierdy_pandu <lifeisubw@gmail.com>
2020-02-09 09:46:27 +0700

    add associations for user model

--- 3.associations_exercise/app/models/user.rb
+++ 3.associations_exercise/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    has_many(:enrollments, {
+        class_name: 'Enrollment',
+        foreign_key: :student_id,
+        primary_key: :id
+    })
+
+    has_many(:enrolled_courses, {
+        through: :enrollments,
+        source: :course
+    })
 end
+
+# an user(student) has many enrollment for a course
