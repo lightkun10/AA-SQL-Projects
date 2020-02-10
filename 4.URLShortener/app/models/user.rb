b5d9e8c8102a8f73de5568ffc4d3e7b18fc525de
fierdy_pandu <lifeisubw@gmail.com>
2020-02-10 08:54:59 +0700

    add association with ShortenedUrl model

--- 4.URLShortener/app/models/user.rb
+++ 4.URLShortener/app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
+
+    has_many(:submitted_urls, {
+        class_name: 'ShortenedUrl',
+        foreign_key: :submitter_id,
+        primary_key: :id
+    })
 end
