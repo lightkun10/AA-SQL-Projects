58d5fc4ea0e92df87f91c94607f5c62e3dc526d6
fierdy_pandu <lifeisubw@gmail.com>
2020-02-10 09:15:16 +0700

    create Visit.record_visit function

--- 4.URLShortener/app/models/visit.rb
+++ 4.URLShortener/app/models/visit.rb
@@ -13,8 +13,8 @@ class Visit < ApplicationRecord
   validates :user_id, :shortened_url_id, presence: true
 
   def self.record_visit!(user, shortened_url)
-    Visits.create!(
-      user_id: user.id, 
+    Visit.create!(
+      user_id: user.id,
       shortened_url_id: shortened_url.id
     )
   end
