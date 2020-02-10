c75fb698170b9befaf665291665d4f5fbb7ef32d
fierdy_pandu <lifeisubw@gmail.com>
2020-02-10 16:26:59 +0700

    add Visit join table model

--- 4.URLShortener/app/models/visit.rb
+++ 4.URLShortener/app/models/visit.rb
@@ -9,8 +9,22 @@
 #  updated_at       :datetime         not null
 #
 
+# a reminder that visit is the 'Join' table
+
 class Visit < ApplicationRecord
   validates :user_id, :shortened_url_id, presence: true
+
+  belongs_to(:shortened_url, {
+    foreign_key: :shortened_url_id,
+    class_name: 'ShortenedUrl',
+    primary_key: :id
+  })
+
+  belongs_to(:visitor, {
+    foreign_key: :user_id,
+    class_name: 'User',
+    primary_key: :id
+  })
 
   def self.record_visit!(user, shortened_url)
     Visit.create!(
