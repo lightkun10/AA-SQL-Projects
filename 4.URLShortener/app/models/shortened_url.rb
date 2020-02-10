d10887894c511c8a43e242c418d822a9beddfd2c
fierdy_pandu <lifeisubw@gmail.com>
2020-02-10 07:48:06 +0700

    add ShortenedUrl::random_code

--- 4.URLShortener/app/models/shortened_url.rb
+++ 4.URLShortener/app/models/shortened_url.rb
@@ -18,7 +18,19 @@ class ShortenedUrl < ApplicationRecord
   validates :short_url, :long_url, :submitter_id, presence: true
   validates :short_url, uniqueness: true
 
-  
+  def self.random_code
+    loop do
+      random_code = SecureRandom.urlsafe_base64(16)
+
+      # Handle the vanishingly small possibility that a code 
+      # has already been taken: keep generating codes until we find 
+      # one that isn't the same as one already stored as the short_url 
+      # of any record in our table. Return the first unused random code.
+      if !ShortenedUrl.exists?(short_url: random_code)
+        return random_code
+      end
+    end
+  end
 
 
 end
