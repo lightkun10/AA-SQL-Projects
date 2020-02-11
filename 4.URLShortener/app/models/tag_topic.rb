8963a9399a2755517a12d01b604ce439a4e97f33
fierdy_pandu <lifeisubw@gmail.com>
2020-02-11 20:28:46 +0700

    add associations and validation for TagTopic model

--- 4.URLShortener/app/models/tag_topic.rb
+++ 4.URLShortener/app/models/tag_topic.rb
@@ -1,3 +1,12 @@
 class TagTopic < ApplicationRecord
   validates :name, presence: true
+
+  has_many :taggings,
+    primary_key: :id,
+    foreign_key: :tag_topic_id,
+    class_name: 'Tagging'
+
+  has_many :shortened_urls,
+    through: :taggings,
+    source: :shortened_url
 end
