4bbc869946f3b608114d4b4c366886447d4b70db
fierdy_pandu <lifeisubw@gmail.com>
2020-02-12 16:37:15 +0700

    add premium boolean column for users table

--- 4.URLShortener/app/models/user.rb
+++ 4.URLShortener/app/models/user.rb
@@ -4,6 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  email      :string           not null
+#  premium    :boolean          default(FALSE)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
